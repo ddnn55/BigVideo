f4423d1fbe5f7b552c480745985fb1eb6c0c43a3
David Stolarsky <david.stolarsky@nytimes.com>
2013-12-06 14:32:55 -0500

    video tile job looks good

---
+++ Tools/XyzTileSequences2XyzTileVideos.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import util
+import os
+import subprocess
+
+source_dir = "xyz_tiles_sequences"
+destination_dir = "xyz_tiles_video"
+
+zooms = util.listdir(source_dir)
+
+for z in zooms:
+	z_path = os.path.join(source_dir, z)
+	cols = util.listdir(z_path)
+
+	for c in cols:
+		c_path = os.path.join(z_path, c)
+		rows = util.listdir(c_path)
+
+		for r in rows:
+			sequence_path = os.path.join(c_path, r)
+			video_destination_dir = os.path.join(destination_dir, z, c)
+			util.ensure_dir(video_destination_dir)
+			destination_path = os.path.join(video_destination_dir, r + ".mp4")
+			cmd = '/usr/bin/env ffmpeg -f image2 -i "' + sequence_path + '/%5d.jpg"  -vcodec libx264 -b 300k "' + destination_path + '"'
+			print cmd
+			#subprocess.call(cmd, shell=True)
--- Tools/util.py
+++ Tools/util.py
@@ -3,3 +3,6 @@ import os
 def ensure_dir(path):
 	if not os.path.exists(path):
 		os.makedirs(path)
+
+def listdir(path):
+	return filter(lambda f: f != ".DS_Store", os.listdir(path))
