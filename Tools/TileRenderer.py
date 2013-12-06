1f8c5b5ac83cb9e01ad3fb3b78dbb65bb53e9e99
David Stolarsky <david.stolarsky@nytimes.com>
2013-12-06 11:01:48 -0500

    higher level render skeleton

--- Tools/TileRenderer.py
+++ Tools/TileRenderer.py
@@ -4,6 +4,7 @@ import util
 from canvas import Canvas
 from geometry import Rect, Point
 from PIL import Image
+import math
 
 TILE_SIZE = 512
 
@@ -25,16 +26,35 @@ class TileRenderer:
 		while pow(2, self.max_zoom) < max(self.grid_size):
 			self.max_zoom = self.max_zoom + 1
 		
-		self.RenderBaseTiles()
-		z = self.max_zoom - 1
-		while z >= 0:
+		base_tiles = self.RenderBaseTiles()
+		self.RenderTilesForAllZoomLevelsUpTo(self.max_zoom - 1, base_tiles)
 
-			self.RenderTilesForZoomLevel(z)
 
-			z = z - 1
-
-	def RenderTilesForZoomLevel(self, z):
+	def RenderTilesForAllZoomLevelsUpTo(self, z, z_plus_1_tiles):
 		print "Rendering tiles for zoom level " + str(z)
+		base_dir = self.output_dir + "/" + str(z)
+		columns = math.ceil(float(z_plus_1_tiles.columns) / 2.0)
+		rows = math.ceil(float(z_plus_1_tiles.rows) / 2.0)
+		for c in range(0, columns):
+			for r in range(0, rows):
+				print "Rendering tile " + str((z, c, r))
+				source_pos = Point(c * TILE_SIZE * 2, r * TILE_SIZE * 2)
+				source_bounds = Rect(source_pos, source_pos + Point(2 * TILE_SIZE, 2 * TILE_SIZE))
+				source_tiles = z_plus_1_tiles.TilesInBounds(source_bounds)
+
+				tile_image = Image.new("RGBA", (TILE_SIZE * 2, TILE_SIZE * 2))
+
+				for source_tile in source_tiles:
+					source_tile_render_pos = source_tile.pos - source_pos
+					tile_image.paste(source_tile.first_frame, source_tile_render_pos.as_tuple())
+
+				tile_image.resize((TILE_SIZE, TILE_SIZE), filter=Image.ANTIALIAS)
+
+				tile_dir = base_dir + "/" + str(c) + "/" + str(r)
+				util.ensure_dir(tile_dir)
+				path = tile_dir + "/0.jpg"
+				base_tile_image.save(path)
+
 
 	def RenderBaseTiles(self):
 		print "Rendering base tiles with max zoom: " + str(self.max_zoom)
